2eb6dd2dad3113c7b18857a404bc5d9f2fec42ec
Brasa Design <diego@brasa.art.br>
2015-02-08 09:58:14 -0200

    home e estrutura do child theme feitas

---
+++ footer.php
@@ -0,0 +1,82 @@
+<?php if ( is_active_sidebar( 'footer-a' ) || is_active_sidebar( 'footer-b') || is_active_sidebar( 'footer-c' ) ) : ?>
+
+	<div class="footer section medium-padding bg-graphite">
+			
+		<div class="section-inner row">
+		
+			<?php if ( is_active_sidebar( 'footer-a' ) ) : ?>
+			
+				<div class="column column-1 one-third">
+				
+					<div class="widgets">
+			
+						<?php dynamic_sidebar( 'footer-a' ); ?>
+											
+					</div>
+					
+				</div>
+				
+			<?php endif; ?> <!-- /footer-a -->
+				
+			<?php if ( is_active_sidebar( 'footer-b' ) ) : ?>
+			
+				<div class="column column-2 one-third">
+				
+					<div class="widgets">
+			
+						<?php dynamic_sidebar( 'footer-b' ); ?>
+											
+					</div> <!-- /widgets -->
+					
+				</div>
+				
+			<?php endif; ?> <!-- /footer-b -->
+								
+			<?php if ( is_active_sidebar( 'footer-c' ) ) : ?>
+			
+				<div class="column column-3 one-third">
+			
+					<div class="widgets">
+			
+						<?php dynamic_sidebar( 'footer-c' ); ?>
+											
+					</div> <!-- /widgets -->
+					
+				</div>
+				
+			<?php endif; ?> <!-- /footer-c -->
+			
+			<div class="clear"></div>
+		
+		</div> <!-- /footer-inner -->
+	
+	</div> <!-- /footer -->
+
+<?php endif; ?>
+
+<div class="credits section light-padding">
+
+	<div class="credits-inner section-inner">
+	
+		<p>
+		
+		&copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="<?php esc_attr( bloginfo('name') ); ?>"><?php bloginfo('name'); ?></a>
+		
+		</p>
+			<div class="clear"></div>
+		<p>
+			
+		<span><?php _e( 'Theme by', 'radcliffe' ); ?> <a href="http://vaz.art.br">VAZ.art.br</a> &mdash; </span><a title="<?php _e('To the top', 'radcliffe'); ?>" href="#" class="tothetop"><?php _e('Up', 'radcliffe' ); ?> &uarr;</a>
+			
+		</p>
+		
+		<div class="clear"></div>
+		
+	</div> <!-- /credits-inner -->
+
+</div> <!-- /credits -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ front-page.php
@@ -0,0 +1,139 @@
+<?php 
+
+/**
+ * Template Name: Home
+ *
+ *
+ * @package Angelo Mundy
+ */
+
+get_header('home'); ?>
+
+<div class="content">	
+
+<div id="site-image"></div>
+
+<div id="site-title">Angelo Mundy</div>
+
+	<div class="header-search-block section light-padding hidden">
+		
+			<div class="section-inner">
+			
+				<form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<input type="search" placeholder="<?php _e('Type and press enter', 'radcliffe'); ?>" name="s" id="s" /> 
+				</form>
+			
+			</div>
+		
+		</div>
+	
+		<div class="header section light-padding">
+		
+			<div class="header-inner section-inner">
+				
+				<div class="nav-toggle">
+				
+					<p><?php _e('Menu','radcliffe') ?></p>
+				
+					<div class="bars">
+					
+						<div class="bar"></div>
+						<div class="bar"></div>
+						<div class="bar"></div>
+						
+						<div class="clear"></div>
+					
+					</div>
+				
+				</div>
+				<ul class="main-menu home">
+					
+					<?php if ( has_nav_menu( 'primary' ) ) {
+																		
+						wp_nav_menu( array( 
+						
+							'container' => '', 
+							'items_wrap' => '%3$s',
+							'theme_location' => 'primary'
+														
+						) ); } else {
+					
+						wp_list_pages( array(
+						
+							'container' => '',
+							'title_li' => ''
+						
+						));
+						
+					} ?>
+											
+				 </ul>
+				<div class="clear"></div>
+			
+			</div> <!-- /header -->
+			
+		</div> <!-- /header.section -->
+		
+		<div class="mobile-menu-container hidden">
+		
+			<ul class="mobile-menu">
+					
+					<?php if ( has_nav_menu( 'primary' ) ) {
+																		
+						wp_nav_menu( array( 
+						
+							'container' => '', 
+							'items_wrap' => '%3$s',
+							'theme_location' => 'primary'
+														
+						) ); } else {
+					
+						wp_list_pages( array(
+						
+							'container' => '',
+							'title_li' => ''
+						
+						));
+						
+					} ?>
+					
+			 </ul>
+			 
+			 <form method="get" class="mobile-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<input type="search" placeholder="<?php _e('Search form', 'radcliffe'); ?>" name="s" id="s" /> 
+					<input type="submit" value="<?php _e('Search', 'radcliffe'); ?>" class="search-button">
+				</form>
+			 
+		</div> <!-- /mobile-menu-container -->
+				
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>				
+	
+		<div <?php post_class('post single'); ?>>
+															    
+		    <div class="post-content section-inner thin">
+		    
+		    	<?php the_content(); ?>
+		    	
+				<div class="clear"></div>
+					
+		    	<?php wp_link_pages('before=<p class="page-links">' . __('Pages:','radcliffe') . ' &after=</p>&seperator= <span class="sep">/</span> '); ?>
+		    
+		    </div>
+
+			<div class="download-link"><a href="#">download do disco abrigação</a></div>
+
+		</div> <!-- /post -->
+		
+	<?php endwhile; else: ?>
+	
+		<p><?php _e("We couldn't find any posts that matched your query. Please try again.", "radcliffe"); ?></p>
+
+	<?php endif; ?>
+
+	<div class="clear"></div>
+
+	
+	
+</div> <!-- /content -->
+								
+<?php get_footer(); ?>
